bacfa8ffd3d696a2cb3a20e6a84062fbf9462339
william rodrigues <williamr723@gmail.com>
2023-08-17 00:59:52 -0300

    Controller

--- tarefas/src/main/java/com/tarefas/controllers/CategoryController.java
+++ tarefas/src/main/java/com/tarefas/controllers/CategoryController.java
@@ -1,13 +1,16 @@
 package com.tarefas.controllers;
 
+import com.tarefas.dtos.CategoryDTO;
 import com.tarefas.entities.Category;
 import com.tarefas.repositories.CategoryRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -18,25 +21,46 @@ public class CategoryController {
 
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
-        var listar = repository.findAll();
-        return ResponseEntity.ok(listar);
+        List<Category> categories = repository.findAll();
+        return ResponseEntity.ok(categories);
     }
     @PostMapping
-    public ResponseEntity<Category> post(@RequestBody Category category){
-        var postar = repository.save(category);
-        return new ResponseEntity<>(postar, HttpStatus.CREATED);
+    public ResponseEntity<Category> post(@RequestBody CategoryDTO categoryDTO){
+        var category = new Category();
+        BeanUtils.copyProperties(categoryDTO, category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(category));
     }
 
-    @PutMapping
-    public ResponseEntity<Category> atualizar(@RequestBody Category category){
-        var atualizar = repository.save(category);
-        return new ResponseEntity<>(atualizar, HttpStatus.OK);
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOne(@PathVariable Long id ){
+        Optional<Category> category = repository.findById(id);
+        if (category.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category not found");
+        }else{
+            return ResponseEntity.status(HttpStatus.OK).body(category);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> atualizar(@RequestBody CategoryDTO categoryDTO,@PathVariable Long id){
+        //Category category = new Category(categoryDTO);
+        Optional<Category> category = repository.findById(id);
+        if (category.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category not found");
+        }
+        var categories = category.get();
+        BeanUtils.copyProperties(categoryDTO, categories);
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(categories));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
-        repository.deleteById(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<Object> delete(@PathVariable Long id){
+        Optional<Category> category = repository.findById(id);
+        if (category.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category not found");
+        }
+        repository.delete(category.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Category deleted successfully");
     }
 
 }
--- tarefas/src/main/java/com/tarefas/controllers/TaskController.java
+++ tarefas/src/main/java/com/tarefas/controllers/TaskController.java
@@ -1,11 +1,16 @@
 package com.tarefas.controllers;
 
+import com.tarefas.dtos.TaskDTO;
 import com.tarefas.entities.Task;
 import com.tarefas.repositories.TaskRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,18 +20,52 @@ public class TaskController {
     private TaskRepository repository;
 
     @GetMapping
-    public List<Task> findAll(){
-        return repository.findAll();
+    public ResponseEntity<List<Task>> findAll(){
+        var listar = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listar);
     }
 
     @PostMapping
-    public Task post(@RequestBody Task task){
-        return repository.save(task);
+    public ResponseEntity<Task> post(@RequestBody TaskDTO taskDTO){
+        var task = new Task();
+        BeanUtils.copyProperties(taskDTO, task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(task));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOne(@PathVariable Long id){
+        Optional<Task> task = repository.findById(id);
+        if (task.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found");
+        }else {
+            return ResponseEntity.status(HttpStatus.OK).body(task);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> atualizar(@RequestBody TaskDTO taskDTO, @PathVariable Long id){
+        //Task task = new Task(taskDTO);
+        Optional<Task> task = repository.findById(id);
+        if (task.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found");
+        }else {
+            var tasks = task.get();
+            BeanUtils.copyProperties(taskDTO, tasks);
+            return ResponseEntity.status(HttpStatus.OK).body(repository.save(tasks));
+        }
+
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
-        repository.deleteById(id);
+    public ResponseEntity<Object> delete(@PathVariable Long id){
+        Optional<Task> task = repository.findById(id);
+        if (task.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found");
+        }else {
+            repository.delete(task.get());
+            return ResponseEntity.status(HttpStatus.OK).body("Task deleted successfully");
+        }
+
     }
 
 }
